a639c554f1181f206ac7d49f3a6dd252c491b038
Yurii <straik123167@gmail.com>
2018-05-24 05:33:34 +0300

    Added lab7

---
+++ Lab7/js/slide.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.slide').slide({
+    isAutoSlide: true,
+    dotsEvent: 'click',
+
+    dotsClass: 'dots',  
+    dotActiveClass: 'active', 
+    arrowClass: 'arrow',
+    arrowLeftClass: 'arrow-left', 
+    arrowRightClass: 'arrow-right'
+
+  });
+});
